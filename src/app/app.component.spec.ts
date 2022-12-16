ba37aa9c200da458bc1f205df391d0353835501f
Wiktor Lemanski <wiktorlem01@gmail.com>
2022-12-16 09:51:37 +0100

    Final version

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Pipes'`, () => {
+  it(`should have as title 'Wage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Pipes');
+    expect(app.title).toEqual('Wage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Pipes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Wage app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'Pipes';
+export class AppComponent implements OnInit {
+  title = 'earn';
+  public earn!: number;
+  public dataTime!: Date;
+  public jsonObj!: any;
+
+  ngOnInit() {
+    timer(0, 1000).subscribe(() => {
+      this.dataTime = new Date();
+    });
+  }
+  decimalFilter(event: any){
+    const reg = /^-?\d*(\.\d*)?$/
+    let input = event.target.value + String.fromCharCode(event.charCode)
+
+    if(!reg.test(input)){
+      event.preventDefault()
+    }
+  }
 }
